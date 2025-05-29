14cd2079e6ec30750fdbea1d6f6fb1000232efba
이성준 <lsj47123qwe@naver.com>
2025-05-29 16:13:12 +0900

    [Feat]깃생성

---
+++ Assets/scripts/PlayerData.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    //플레이어 스태이터스 관리 
+    //HP,HP재생 공격력 방어력 속도 스킬가속%
+
+    public int PlayerHp = 10;
+    public int PlayerReHP = 10;
+    public int PlayerATK = 5;
+    public int PlayerDP = 5;
+    public int PlayerSPD = 3;
+    public int PlayerCT = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
